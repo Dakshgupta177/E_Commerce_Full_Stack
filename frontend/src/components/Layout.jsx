f15bda6798b2451e1e21771ac97376dfb899139d
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-06-25 21:29:30 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -19,7 +19,7 @@ const Layout = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        credentials: "include",
+        withCredentials: true,
       });
       console.log(response);
       
