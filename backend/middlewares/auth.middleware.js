15b91f86c37ccccc447b61e4ad000d2783e061a2
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-06-26 13:48:22 +0530

    Update auth.middleware.js

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -6,8 +6,9 @@ export const verifyJWT = async (req, res, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization").replace("Bearer ", "");
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
+    console.log("❌ No token found in cookies or headers");
+    return res.status(400).json({ message: "Token not found" });
+  }
     const decodedUser = jwt.verify(token, process.env.ACCESS_TOKEN_KEY);
     const user = await User.findById(decodedUser._id).select(
       "-password -refreshToken"
