6922c377e6066a727de32a5d595dd732d6141ad5
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-06-27 19:51:40 +0530

    Update Success.jsx

--- frontend/src/components/Success.jsx
+++ frontend/src/components/Success.jsx
@@ -35,7 +35,7 @@ const Success = () => {
       {loading && (
         <img
           src="https://i.gifer.com/ZKZg.gif"
-          className="size-12 fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
+          className="size-12 fixed top-1/2 left-1/2 z-50"
           alt="Loading..."
         />
       )}
