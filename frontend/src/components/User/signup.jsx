3187a8a25b6695451cb2d5136f89402af6297546
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-07-07 12:02:11 +0530

    Update signup.jsx

--- frontend/src/components/User/signup.jsx
+++ frontend/src/components/User/signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
     setsubmit(true);
     try {
       const dat = await axios.post(
-        "https://e-commerce-full-stack-backend-7by3.onrender.com/api/user/signup",
+        "/api/user/signup",
         data, {
         headers: {
           "Content-Type": "application/json",
